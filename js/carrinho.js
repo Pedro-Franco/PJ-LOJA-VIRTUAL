ab6b334f1485483549391bb88c0619281e7c3a78
Pedro Franco <80595514+Pedro-Franco@users.noreply.github.com>
2022-06-23 18:33:00 -0300

    Update carrinho.js

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,25 +1,3 @@
-//função que adiciona itens ao carrinho de compras
-// addCarrinho = () => {
-//   var id_produto = document.getElementsByClassName('id').value
-//   var nome_produto = document.getElementsByClassName('produto').value
-//   var valor_produto = document.getElementsByClassName('valor').value
-
-//   if (id_produto === estoqueProdutosObj.id || nome_produto === estoqueProdutosObj.nome || valor_produto === estoqueProdutosObj.valor) {
-//     var containerCarriho = document.getElementById('tabela')
-//       document.innerHTML += `
-//       <tr>
-//         <th scope="row">${id_produto}</th>
-//         <td>${nome_produto}</td>
-//         <td>1</td>
-//         <td>${valor_produto}</td>
-//       </tr>
-//       `
-//   } else {
-//     console.log('passou por aqui')
-//   }
-  
-// }
-
 var total = 0 // retorna o total dos produtos que estao na localstorage
 var i = 0 // variavel que ira percorrer as posições
 var total = 0 // variavel que irá receber o preço dos produtos convertido em float
